cbe8160dd85ed99039e3cde51678d434edc51e3e
ATursunbekov <tursunbekov_a@auca.kg>
2024-02-04 14:46:12 +0600

    initial commit

---
+++ iOS_client_NeoCafe_/iOS_client_NeoCafe_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_client_NeoCafe_
+//
+//  Created by Alikhan Tursunbekov on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
---
+++ iOS_waiter_NeoCafe/iOS_waiter_NeoCafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_waiter_NeoCafe
+//
+//  Created by Alikhan Tursunbekov on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

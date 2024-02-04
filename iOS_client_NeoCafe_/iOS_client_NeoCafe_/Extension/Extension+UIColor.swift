56dbf78b13ec9d2d771c10ec12df0abb9e826020
ATursunbekov <tursunbekov_a@auca.kg>
2024-02-04 15:09:50 +0600

    Setup project

---
+++ iOS_client_NeoCafe_/iOS_client_NeoCafe_/Extension/Extension+UIColor.swift
@@ -0,0 +1,25 @@
+//
+//  Extension+UIColor.swift
+//  iOS_client_NeoCafe_
+//
+//  Created by Alikhan Tursunbekov on 4/2/24.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
